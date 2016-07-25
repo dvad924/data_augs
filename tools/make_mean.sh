ebca4b0356c94431ceb6afc412cb9f83c386476b
david vadney <dvad924@gmail.com>
2016-07-25 11:08:58 +0800

    ran experiment with slightly more people than previously, results seem similar

--- tools/make_mean.sh
+++ tools/make_mean.sh
@@ -5,7 +5,7 @@
 DATA=data/person_only_lmdb
 TOOLS=$CAFFE_ROOT/build/tools
 
-$TOOLS/compute_image_mean $DATA/people_color_patch_train_lmdb \
-  $DATA/person_color_patch_color_mean.binaryproto
+$TOOLS/compute_image_mean $DATA/person_background_only_train_lmdb \
+  $DATA/person_background_only_color_mean.binaryproto
 
 echo "Done."
--- tools/person_src/sep_classes.sh
+++ tools/person_src/sep_classes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #store class 0 elements
-grep ' 0' ../assign/patch_test.txt ../assign/patch_train.txt | sed 's/.*://' > background.txt
-grep ' 1' ../assign/patch_test.txt ../assign/patch_train.txt | sed 's/.*://' > person.txt
-grep ' 2' ../assign/patch_test.txt ../assign/patch_train.txt | sed 's/.*://' > car.txt
-grep ' 3' ../assign/patch_test.txt ../assign/patch_train.txt | sed 's/.*://' > random.txt
+grep ' 0' assign/patch_test.txt assign/patch_train.txt | sed 's/.*://' > assign/background.txt
+grep ' 1' assign/patch_test.txt assign/patch_train.txt | sed 's/.*://' > assign/person.txt
+grep ' 2' assign/patch_test.txt assign/patch_train.txt | sed 's/.*://' > assign/car.txt
+grep ' 3' assign/patch_test.txt assign/patch_train.txt | sed 's/.*://' > assign/random.txt
