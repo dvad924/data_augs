f95bd16b779198c473731b6cb26b0f2473c54ccd
David Vadney <dvad924@gmail.com>
2016-07-26 11:21:42 +0800

    added some batch ability to detector still need to finish

--- tools/detector.py
+++ tools/detector.py
@@ -8,8 +8,10 @@ import os
 import detector_utils as DU
 import argparse
 import detconfig as cfg
+import skimage.data
+
 class MyCaffeNet:
-    def __init__(self,net_arch,weights,mode,mean=None,scale=256):
+    def __init__(self,net_arch,weights,mode,mean=None,scale=256,shape=None):
         self.arch = net_arch
         self.weights = weights
         self.mode = mode
@@ -17,13 +19,17 @@ class MyCaffeNet:
         self.scale = scale
         if self.meanfile:
             self.set_mean()
-        self.prepare_net()
+        if shape:
+            self.prepare_net(shape=shape)
+        else:
+            self.prepare_net()
         self.set_transform()
         
-    def prepare_net(self):
+    def prepare_net(self,shape=(1,3,128,128)):
         caffe.set_mode_cpu()
         self.net = caffe.Net(self.arch,self.weights,self.mode)
-        self.net.blobs['data'].reshape(1,3,128,128)
+        depth,channels,height,width = shape
+        self.net.blobs['data'].reshape(depth,channels,height,width)
         
     def set_mean(self):
         blob = caffe.proto.caffe_pb2.BlobProto()
@@ -45,20 +51,53 @@ class MyCaffeNet:
     def load_image(self,imgname):
         img = caffe.io.load_image(imgname)
         return self.transformer.preprocess('data',img)
-        
+    
+    def load_and_propose(self,imgname):
+        img = caffe.io.load_image(imgname);
+        #this will be a time bottleneck
+        props = DU.selective_window(img)
+        return img,props
     def set_img(self,img):
         self.net.blobs['data'].data[...] = img
 
-    def run_image(self,imgname):
+    def set_imgs(self,imgs):
+        for i,img in enumerate(imgs):
+            self.net.blobs['data'].data[i,...] = img
+            
+    def run_image(self,imgname,logfile=None):
         img = self.load_image(imgname)
         self.set_img(img)
         self.net.forward()
         fname = imgname.split('/')[-1]
         clas = self.net.blobs[cfg.out].data.argmax(1)
         prob = self.net.blobs['cls_prob'].data
-        print '{} class:{}, prob:{}\n'.format(fname,clas,prob)
+        val = '{} class:{}, prob:{}\n'.format(fname,clas,prob)
+        print val
+        if logfile:
+            logfile.write('{}\n'.format(val))
         return clas[0]
+
+    def run_batch(self,imgs,logfile=None):
+        self.set_imgs(imgs) #set the images into the network batch
+        self.net.forward()  #run the batch through the network
+        clas = self.net.blobs[cfg.out].data.argmax(1) #grab the assigned class labels
+        prob = self.net.blobs['cls_prob'].data
+        return clas,prob
     
+    def propose_and_detect(self,imgname,logfile=None):
+        img,props = self.load_and_propose(imgname)
+        rects = map(lambda x:x['rect'],props)
+        images = [img[y:y+h,x:x+w,:] for x,y,w,h in rects]
+        batchsize,c,h,w = self.net.blobs['data'].data.shape
+        for i in xrange(0,np.ceil(float(len(images)/(batchsize*1.0)))):
+            l = i * batchsize
+            u = min((i+1) * batchsize,len(images))
+            batch = images[l:u]
+            if len(batch) < batchsize:
+                self.net.blobs['data'].reshape(len(batch),c,h,w)
+            clas,prob =  self.run_batch(batch)
+        
+        
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('net')
@@ -70,23 +109,32 @@ def parseArgs():
     parser.add_argument('--list')
     return parser.parse_args()
 
-def test(dirr,infile,outfile,neg='__background',pos='person'):
-    files2 = []    
+def test(dirr,infile,outfile,clasmap={'__background':0,'person':1}):
+    files2 = []
+    classes = []
+    logfile = outfile+'.log'
+    lfile = open(logfile,'a')
     with open(infile) as fil:
         for line in fil.readlines():
             fields = line.split(' ')
             files2.append(fields[0].strip())
+            if fields[1].strip():
+                classes.append(int(fields[1].strip()))
 
     accs = np.zeros((len(files2),))
     daccs = np.zeros((len(files2),))
-    
+    if len(classes) > 0:
+        daccs = np.array(classes)
+        usename = False
     for i ,f in enumerate(files2):
-        accs[i] = mynet.run_image(os.path.join(dirr,f))
-        if f.find(neg)>=0 :
-            daccs[i] = 0
-        elif f.find(pos) >=0 :
-            daccs[i] = 1
-        print i, daccs[i]
+        accs[i] = mynet.run_image(os.path.join(dirr,f),logfile=lfile)
+   
+        if usename:
+            for key in clasmap.keys():
+                if f.startswith(key) :
+                    daccs[i] = clasmap[key]
+                    break
+            print i, daccs[i]
         
     pdb.set_trace()
     val = 'final acc:{}'.format(np.sum(accs==daccs)/(len(files2)*1.0))
@@ -99,10 +147,20 @@ if __name__ == '__main__':
     import pdb
     args = parseArgs()
     mynet = MyCaffeNet(args.net,args.weights,caffe.TEST,args.mean)
+    ###########################random objects test###############
     dirr = '/Users/dvad/image_patches/'
-    assigndir = '/Users/dvad/image_patches/assign'
-    test(dirr,os.path.join(assigndir,'person_only_train.txt'),'train.txt')
+    assigndir = '/Users/dvad/image_patches/assign/random.txt'
+    test(dirr,assigndir,'randomresults.txt',)
+    ###########################peta data test######################
+    # dirr = '/Volumes/DVBackBox/PetaData/PETA/orgdata/images'
+    # assigndir = '/Volumes/DVBackBox/PetaData/PETA/orgdata/assign'
+    # test(dirr,os.path.join(assigndir,'labels.txt'),'petaresults.txt')
+    ###########################Training Set Test###################
+    # dirr = '/Users/dvad/image_patches/'
+    # assigndir = '/Users/dvad/image_patches/assign'
+    # test(dirr,os.path.join(assigndir,'person_only_train.txt'),'train.txt')
     #train val
+    ###########################Testing Set Test####################
     # with open(dirr+'assign/person_only_test.txt') as fil:
     #     for lin in fil.readlines():
     #         fields = lin.split(' ')
--- tools/detector_utils.py
+++ tools/detector_utils.py
@@ -1,2 +1,52 @@
 import numpy as np
 import os
+import cv2
+from selectivesearch import selective_search
+testdir = '/Users/dvad/code/deepLearning/peta/src/actions3/orgdata/images/'
+
+
+def sliding_window(img,window=(138,60),hstep=25,wstep=25):
+    #get image dimmensions
+    rows,cols,channels =  img.shape
+    #get dimensions of the window to use
+    height,width = window
+    #get starting coors of the window save final boarders
+    ys = np.arange(0,rows-height-1,hstep)
+    xs = np.arange(0,cols-width-1,wstep)
+
+    #tack the final possible positions on the end
+    ys = np.append(ys,rows-height-1)
+    xs = np.append(xs,cols-width-1)
+    # this will scan from left 2 right,
+    # not the fastest implementation but should be plenty for this application
+    points = [(y,x) for y in ys for x in xs]
+    
+
+    frames = [img[y:y+height,x:x+width] for (y,x) in points]
+
+    return frames
+    
+def selective_window(img,scale=300,sigma=0.8,mins=20):
+    #this is a time bottleneck
+    im_lables,regions = selectivesearch(img,scale=scale,sigma=sigma,min_size=mins);
+
+    true_regions = set()
+    for reg in region:
+        if reg in true_regions:
+            continue
+        if reg['size'] < 100:
+            continue
+        true_regions.add(reg['rect'])
+    return true_regions
+
+
+
+if __name__ == '__main__':
+    import pdb
+    pdb.set_trace()
+    img = cv2.imread(os.path.join(testdir,'1001.jpg'))
+    stack = sliding_window(img,(138,60),25,25)
+    print len(stack)
+    for im in stack:
+        cv2.imshow('win1',im)
+        cv2.waitKey(33)
